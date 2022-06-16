fde6e23982f78d128b0eef95efee78200d6e2eea
"Anthony" <"aschyns499@gmail.com">
2022-06-16 17:09:23 +0200

    FundMe

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -2,35 +2,53 @@
 
 pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 50;
+    uint256 public minimumUsd = 50 * 1e18;
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
+    address public owner;
+
+    //constrtor is called when we deploy the contract
+
+    constructor() {
+        owner = msg.sender;
+    }
 
  function fund() public payable {
      // Send eth to the contract
-     require(msg.value > minimumUsd, "Didn't send enough"); // 1e18 = 1eth in wei
+     require(msg.value.getConversionRate() > minimumUsd, "Didn't send enough"); // 1e18 = 1eth in wei
      // gas spend after require is give back if require isn't met
- }
-
- function getPrice() public view returns(uint256) {
-     // 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
-     AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-     (, int256 price,,,) = priceFeed.latestRoundData();
-
-     return uint256(price * 1e10);
- }
-
- function getVersion() public view returns (uint256) {
-     AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-     return priceFeed.version();
- }
-
- function getConversionRate(ethAmount) public view returns(uint256) {
+     funders.push(msg.sender);
+     addressToAmountFunded[msg.sender] += msg.value;
 
  }
 
- // function withdraw() {}
+ function withdraw() public onlyOwner {
+     for (uint256 funderIndex; funderIndex < funders.length; funderIndex++) {
+         address funder = funders[funderIndex];
+         addressToAmountFunded[funder] = 0;
+     }
+     funders = new address[](0);
+     //transfer
+     //payable(msg.sender).transfer(address(this).balance);
+     //send
+    // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+     //require(sendSuccess, "Send Failed");
+     //call
+     (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+     require(callSuccess, "Call Failed");
+ }
+
+ modifier onlyOwner {
+     require(msg.sender == owner, "Sender is not owner");
+     //then continuer code
+     _;
+ }
 
 }
